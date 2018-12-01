678f9bb0b3e4831d545d502553032b8b49e80d90
mhk29 <42727181+mhk29@users.noreply.github.com>
2018-12-01 18:46:27 -0500

    Add files via upload

---
+++ PercolationBFS.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PercolationBFS extends PercolationDFSFast {
+	
+	public PercolationBFS(int n) {
+		super(n);
+	}
+	
+	@Override 
+	public void dfs(int row, int col) {
+		
+		if (! inBounds(row, col)) return;
+		if (isFull(row, col) || ! isOpen(row, col)) return;
+		
+		myGrid[row][col] = FULL;
+		Queue<Integer> que = new LinkedList<Integer>();
+		que.add(row*myGrid.length + col);
+		
+		while (que.size() != 0) {
+			
+			int helper = que.remove();
+			row = recover(helper, myGrid.length)[0];
+			col = recover(helper, myGrid.length)[1];
+			
+			if (inBounds(row - 1, col) && isOpen(row - 1, col) && ! isFull(row - 1, col)) {
+				myGrid[row - 1][col] = FULL;
+				que.add((row - 1) * myGrid.length + col);
+			}
+			if (inBounds(row + 1, col) && isOpen(row + 1, col) && ! isFull(row + 1, col)) {
+				myGrid[row + 1][col] = FULL;
+				que.add(((row + 1) * myGrid.length) + col);
+			}
+			if (inBounds(row, col - 1) && isOpen(row, col - 1) && ! isFull(row, col - 1)) {
+				myGrid[row][col - 1] = FULL;
+				que.add(row*myGrid.length + col - 1);
+			}
+			if (inBounds(row, col + 1) && isOpen(row, col + 1) && ! isFull(row, col + 1)) {
+				myGrid[row][col + 1] = FULL;
+				que.add(row*myGrid.length + col + 1);	
+			}
+		}
+	}
+	
+	public int[] recover (int n, int m) {
+		int row = n / m;
+		int col = n % m;
+		return new int[] {row, col};
+	}
+	
+}
